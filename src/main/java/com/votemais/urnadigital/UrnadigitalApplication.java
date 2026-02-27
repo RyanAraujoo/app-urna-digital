f97327bd90b78a2bc962333398137131a794aa98
Ryan Araujo <ryan.pablo.silva.araujo@gmail.com>
2026-02-26 22:13:43 -0300

    first commit project

---
+++ src/main/java/com/votemais/urnadigital/UrnadigitalApplication.java
@@ -0,0 +1,13 @@
+package com.votemais.urnadigital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrnadigitalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrnadigitalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/votemais/urnadigital/UrnadigitalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.votemais.urnadigital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrnadigitalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
